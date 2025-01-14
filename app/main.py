25963599f5b9e98704df1d01c31b6518349667c9
udit01 <uditjainiitd@gmail.com>
2025-01-14 22:12:58 +0530

    playing around with some byte encodings

--- app/main.py
+++ app/main.py
@@ -8,8 +8,31 @@ def main():
 
     # Uncomment this to pass the first stage
     #
+
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    
+    # while(True): 
+    client_socket, client_address = server.accept() # wait for client
+
+    encoding = 'utf-16'
+    
+    message_size = 1
+    corr_id = 7
+    header = corr_id
+    body = ''
+
+    # response = "%d%d"%(message_size, corr_id)
+    # print(response)
+    # response = bytes(response, 'utf-32')
+    # print(response)
+    response = bytes('0', encoding)
+    response += bytes('7', encoding)
+    # print(response)
+
+    # response = b'0001\n0007'
+    client_socket.send(response)
+
+    
 
 
 if __name__ == "__main__":
