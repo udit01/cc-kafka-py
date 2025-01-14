ad85e1b75531eacdb19e19b2e9c3395d83b96901
udit01 <uditjainiitd@gmail.com>
2025-01-14 22:57:49 +0530

    implemented the ApiVer request for 1 error code

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 import socket  # noqa: F401
 import base64
 
-def message(cid):
-    idByte = cid.to_bytes(4, byteorder="big")
-    header = len(idByte).to_bytes(4, byteorder="big")
-    return header + idByte
+def message(cid, error_code):
+    # First have the correlation id in 4 bytes
+    payload = cid.to_bytes(4, byteorder="big")
+    # Then the error code for the request in 2 bytes
+    payload += error_code.to_bytes(2, byteorder="big")
+    # total message size will the the size of the payload in bytes and will be stored in the first 4 bytes
+    message_size = len(payload).to_bytes(4, byteorder="big")
+    return message_size + payload
 
 def parse_request(req): 
     message_size = int.from_bytes( (req[:4]), byteorder='big')
@@ -14,13 +18,18 @@ def parse_request(req):
 
     print("client reponse had : ")
     print(message_size, request_api_key, request_api_ver, correlation_id)
-    return correlation_id
+    return message_size, request_api_key, request_api_ver, correlation_id
     
 
 def handle_client(client):
     client_request = client.recv(1024)
-    corr_id = parse_request(client_request)
-    client.sendall(message(corr_id))
+    msg_sz, req_api_key, req_api_ver, corr_id = parse_request(client_request)
+    
+    # API key will be 18 as the ApiVersionsRequest
+    assert req_api_key == 18
+    
+    error_code = 0 if (req_api_ver == 4)  else 35
+    client.sendall(message(corr_id, error_code=error_code))
     client.close()
 
 def main():
