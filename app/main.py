20466dff178ba8b31b60a27ccd31bed95a09f3b8
udit01 <uditjainiitd@gmail.com>
2025-01-16 00:09:58 +0530

    trying to do conc with 2 loops

--- app/main.py
+++ app/main.py
@@ -46,45 +46,22 @@ def make_response(request: KafkaRequest):
     response_length = len(response_header) + len(response_body)
     return int(response_length).to_bytes(4, byteorder='big') + response_header + response_body
 
-# def message(cid, error_code):
-#     # First have the correlation id in 4 bytes
-#     payload = cid.to_bytes(4, byteorder="big")
-#     # Then the error code for the request in 2 bytes
-#     payload += error_code.to_bytes(2, byteorder="big")
-#     # total message size will the the size of the payload in bytes and will be stored in the first 4 bytes
-#     message_size = len(payload).to_bytes(4, byteorder="big")
-#     return message_size + payload
 
-# def parse_request(req): 
-#     message_size = int.from_bytes( (req[:4]), byteorder='big')
-#     request_api_key = int.from_bytes( (req[4:6]), byteorder='big')
-#     request_api_ver = int.from_bytes( (req[6:8]), byteorder='big')
-#     correlation_id = int.from_bytes( (req[8:12]), byteorder='big')
-
-#     print("client reponse had : ")
-#     print(message_size, request_api_key, request_api_ver, correlation_id)
-#     return message_size, request_api_key, request_api_ver, correlation_id
-    
-
-# def handle_client(client):
-#     # client_request = client.recv(1024)
-#     client_request = KafkaRequest.from_client(client)
-#     print(client_request)    
-#     response = make_response(client_request)
-#     print(response)
-#     client.sendall(response)
-#     # client.close()
-
-def main():
-    # print("Logs from your program will appear here!")
-    server = socket.create_server(("localhost", 9092), reuse_port=True)
+def client_loop(client):
     while True:
-        client, addr = server.accept()
         client_request = KafkaRequest.from_client(client)
         print(client_request)    
         response = make_response(client_request)
         print(response)
         client.sendall(response)
+
+def main():
+    # print("Logs from your program will appear here!")
+    server = socket.create_server(("localhost", 9092), reuse_port=True)
+    while True: 
+        client, addr = server.accept()
+        client_loop(client)
+
 
     
 
